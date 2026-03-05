e8b55d65d22fcb9f5d6fbc19e1a7b4c72b3b4e2b
Enrique Cametti <me@quiquecmtt.com>
2026-03-05 01:12:28 -0300

    chore(sec): modifications to pass checkov tests

--- route53_delegated.tf
+++ route53_delegated.tf
@@ -10,3 +10,21 @@ resource "aws_route53_zone" "delegated_zone" {
     tofuModule = "route53-to-route53-delegation"
   }
 }
+
+resource "aws_route53_hosted_zone_dnssec" "delegated_zone_dnssec" {
+  count          = var.enable_dnssec ? 1 : 0
+  hosted_zone_id = aws_route53_zone.delegated_zone.zone_id
+}
+
+resource "aws_route53_key_signing_key" "delegated_zone_dnssec" {
+  count                      = var.enable_dnssec ? 1 : 0
+  name                       = "delegated-zone-${replace(var.route53_delegated_zone, ".", "-")}"
+  hosted_zone_id             = aws_route53_zone.delegated_zone.zone_id
+  key_management_service_arn = var.dnssec_kms_key_arn
+}
+
+resource "aws_route53_query_log" "delegated_zone_query_log" {
+  count                    = var.enable_query_logging ? 1 : 0
+  zone_id                  = aws_route53_zone.delegated_zone.zone_id
+  cloudwatch_log_group_arn = var.query_logging_cloudwatch_log_group_arn
+}
--- variables.tf
+++ variables.tf
@@ -11,3 +11,29 @@ variable "route53_delegated_zone" {
   nullable    = false
   sensitive   = false
 }
+
+variable "enable_dnssec" {
+  description = "Enable DNSSEC signing for the delegated zone"
+  type        = bool
+  default     = false
+}
+
+variable "dnssec_kms_key_arn" {
+  description = "KMS key ARN for DNSSEC signing. Required if enable_dnssec is true"
+  type        = string
+  default     = null
+  sensitive   = false
+}
+
+variable "enable_query_logging" {
+  description = "Enable DNS query logging for the delegated zone"
+  type        = bool
+  default     = false
+}
+
+variable "query_logging_cloudwatch_log_group_arn" {
+  description = "CloudWatch log group ARN for DNS query logging. Required if enable_query_logging is true"
+  type        = string
+  default     = null
+  sensitive   = false
+}
